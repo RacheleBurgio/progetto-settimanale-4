47ef0b9c89c6322104a39ff013a8877e793c64ee
RacheleBurgio <rachele.burgio32@gmail.com>
2024-11-03 19:29:33 +0100

    altre modifiche

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,35 +1,4 @@
-window.onscroll = function () {
-  const head = document.getElementById('header')
-  if (head && window.scrollY > 500) {
-    head.style.backgroundColor = '#ffffff'
-  } else {
-    head.style.backgroundColor = '#ffc017'
-  }
-  const button = document.getElementById('curios')
-  if (button && window.scrollY > 500) {
-    button.style.backgroundColor = '#538724'
-  } else {
-    button.style.backgroundColor = '#000000'
-  }
-}
-
-window.onload = function () {
-  const svg = document.getElementById('svg-image')
-  if (svg) {
-    const paths = document.getElementsByTagName('g')
-    const butts = []
-    for (let i = 0; i < paths.length; i++) {
-      if (paths[i].getAttribute('stroke-linecap') === 'butt') {
-        butts.push(paths[i])
-      }
-    }
-    setInterval(function () {
-      animateSvg(butts)
-    }, 50)
-  }
-}
-
-function animateSvg(butts) {
+const animateSvg = (butts) => {
   const index = Math.ceil(Math.random() * butts.length)
   if (index < butts.length) {
     if (butts[index].getAttribute('opacity') === '1') {
@@ -40,15 +9,34 @@ function animateSvg(butts) {
   }
 }
 
+window.onload = () => {
+  const svg = document.getElementById('svg-image')
+  if (svg) {
+    const paths = document.getElementsByTagName('g')
+    const butts = []
+    for (let i = 0; i < paths.length; i++) {
+      if (paths[i].getAttribute('stroke-linecap') === 'butt') {
+        butts.push(paths[i])
+      }
+    }
+    setInterval(() => {
+      animateSvg(butts)
+    }, 50)
+  }
+}
+
 let myNav = document.getElementById('nav')
+let mybutt = document.getElementById('verde')
 
 window.onscroll = () => {
   if (
-    document.body.scrollTop >= 500 ||
-    document.documentElement.scrollTop >= 500
+    document.body.scrollTop >= 400 ||
+    document.documentElement.scrollTop >= 400
   ) {
     myNav.classList.add('scroll')
+    mybutt.style.backgroundColor = '#1a8917'
   } else {
     myNav.classList.remove('scroll')
+    mybutt.style.backgroundColor = '#191919'
   }
 }
